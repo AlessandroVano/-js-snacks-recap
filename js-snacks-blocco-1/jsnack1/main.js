d6864ec83f658dfb4d797e9bb3c0cafeca54df79
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-30 12:39:53 +0200

    inserimento istruzioni condizionali (parole con la stessa lunghezza, prima parola maggiore della 2°

--- js-snacks-blocco-1/jsnack1/main.js
+++ js-snacks-blocco-1/jsnack1/main.js
@@ -8,4 +8,15 @@ il software stampa la prima parola più corta, poi la parola più lunga
 
 // INSERIMENTO PAROLE
 const parola1 = prompt('inserisci la prima parola');
+console.log(parola1);
 const parola2 = prompt('inserisci la seconda parola');
+console.log(parola2);
+
+
+// stampo prima parola più corta e successivamente quella più lunga
+
+if (parola1 === parola2) {
+    console.log('hanno la stessa lunghezza');
+} else if (parola1 > parola2) {
+    console.log(`${parola2} ${parola1}`);
+}
