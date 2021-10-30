e0097c709664b0b93e25d0acc5cd4f2886883298
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-30 12:29:21 +0200

    jsnack1 parole in successione

---
+++ js-snacks-blocco-1/jsnack1/main.js
@@ -0,0 +1,11 @@
+/**
+ * 
+Jsnack 1
+
+L'utente inserisce due parole in successione con due prompt.
+il software stampa la prima parola più corta, poi la parola più lunga
+ */
+
+// INSERIMENTO PAROLE
+const parola1 = prompt('inserisci la prima parola');
+const parola2 = prompt('inserisci la seconda parola')
