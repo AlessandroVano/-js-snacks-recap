f7b062f0732a02bb689ddffc5ca94a45f4cf056f
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-30 12:30:43 +0200

    inserimento parole

--- js-snacks-blocco-1/jsnack1/main.js
+++ js-snacks-blocco-1/jsnack1/main.js
@@ -8,4 +8,4 @@ il software stampa la prima parola più corta, poi la parola più lunga
 
 // INSERIMENTO PAROLE
 const parola1 = prompt('inserisci la prima parola');
-const parola2 = prompt('inserisci la seconda parola')
+const parola2 = prompt('inserisci la seconda parola');
