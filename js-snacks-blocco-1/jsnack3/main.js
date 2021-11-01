091879175097f98cd1f09a300353e09f265efeae
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-01 10:55:19 +0100

    Jsnack 3  blocco 1 , inserimento array vuoto + richiesta di inserimento numero

---
+++ js-snacks-blocco-1/jsnack3/main.js
@@ -0,0 +1,15 @@
+/**
+ * Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array
+ */
+
+// inserimento array vuoto
+const dispari = [];
+
+//richiesta di inserimento numero 
+for (let i = 0; i < 6; i++) {
+    const richiestaNumeri = parseInt( prompt('inserisci un numero'));
+    console.log(richiestaNumeri);
+   
+} 
